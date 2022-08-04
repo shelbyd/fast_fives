401509dceff30df5f8a6fb680b5b25cf115e950b
Shelby Doolittle <shelby@shelbyd.com>
2022-08-04 09:55:52 -0400

    Use bits to compare words

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use indicatif::ParallelProgressIterator;
+#![allow(unused_import)]
+
+use indicatif::{ProgressIterator, ParallelProgressIterator};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
-use std::collections::HashSet;
+use std::{cmp::Ordering, collections::HashSet};
 
 fn main() {
     let file = std::env::args()
@@ -24,15 +26,60 @@ fn main() {
     eprintln!("Found {} solutions", result.len());
 }
 
-fn word_sets_root<'w>(sorted_options: &[&'w str]) -> Vec<Vec<&'w str>> {
-    sorted_options
+#[derive(Clone, Copy)]
+struct Word<'w> {
+    string: &'w str,
+    bits: u32,
+}
+
+impl<'w> Word<'w> {
+    fn new(string: &'w str) -> Self {
+        let is_ascii = string.chars().all(|c| c.is_ascii_lowercase());
+        if !is_ascii {
+            panic!("Requires ascii string got {}", string);
+        }
+
+        let mut bits = 0;
+        for b in string.bytes() {
+            let index = b - b'a';
+            let bit = 1 << index;
+            if bits & bit != 0 {
+                panic!("Found duplicate characters {}", string);
+            }
+            bits |= bit;
+        }
+
+        Word { string, bits }
+    }
+
+    fn overlaps(&self, other: Self) -> bool {
+        self.bits & other.bits != 0
+    }
+}
+
+// Orders based on bit strings.
+impl<'w> PartialOrd for Word<'w> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.bits.partial_cmp(&other.bits)
+    }
+}
+
+impl<'w> PartialEq for Word<'w> {
+    fn eq(&self, other: &Self) -> bool {
+        self.bits == other.bits
+    }
+}
+
+fn word_sets_root<'w>(options: &[&'w str]) -> Vec<Vec<&'w str>> {
+    let options = options.iter().map(|&w| Word::new(w)).collect::<Vec<_>>();
+    options
         .par_iter()
         .progress()
-        .flat_map(|word| {
-            unique_word_sets(&filter_valid_options(sorted_options, word), 4)
+        .flat_map(|&word| {
+            unique_word_sets(&filter_valid_options(&options, word), 4)
                 .into_iter()
                 .map(|mut set| {
-                    set.insert(0, *word);
+                    set.insert(0, word.string);
                     set
                 })
                 .collect::<Vec<_>>()
@@ -40,28 +87,29 @@ fn word_sets_root<'w>(sorted_options: &[&'w str]) -> Vec<Vec<&'w str>> {
         .collect()
 }
 
-fn filter_valid_options<'w>(options: &[&'w str], word: &str) -> Vec<&'w str> {
+fn filter_valid_options<'w>(options: &[Word<'w>], word: Word<'w>) -> Vec<Word<'w>> {
     options
         .iter()
+        // Order of these matters a lot. overlaps is much faster than comparison.
+        .filter(|w| !w.overlaps(word))
         .filter(|&w| *w > word)
-        .filter(|w| w.chars().all(|c| !word.contains(c)))
         .map(|&w| w)
         .collect()
 }
 
-fn unique_word_sets<'w>(valid_options: &[&'w str], needed_words: u8) -> Vec<Vec<&'w str>> {
+fn unique_word_sets<'w>(valid_options: &[Word<'w>], needed_words: u8) -> Vec<Vec<&'w str>> {
     if needed_words == 1 {
-        return valid_options.iter().map(|&w| vec![w]).collect();
+        return valid_options.iter().map(|&w| vec![w.string]).collect();
     }
 
     valid_options
         .iter()
-        .flat_map(|option| {
+        .flat_map(|&option| {
             let next_options = filter_valid_options(valid_options, option);
             unique_word_sets(&next_options, needed_words - 1)
                 .into_iter()
                 .map(|mut set| {
-                    set.insert(0, option);
+                    set.insert(0, option.string);
                     set
                 })
         })
